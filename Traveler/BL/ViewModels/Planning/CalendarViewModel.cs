fbbb16527363603c66f4bc8d52b826039361cf6e
Максим Веркошанский <profillive@mail.ru>
2019-04-12 18:57:20 +0300

    command added

--- Traveler/BL/ViewModels/Planning/CalendarViewModel.cs
+++ Traveler/BL/ViewModels/Planning/CalendarViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Traveler.BL.ViewModels.Planning
 {
     class CalendarViewModel : BaseViewModel
     {
+        public ICommand GoToTravelNameCommand => GetNavigateToCommand(AppPages.TravelName);
+        public ICommand GoToEventsListCommand => GetNavigateToCommand(AppPages.EventsList);
     }
 }
--- Traveler/BL/ViewModels/Planning/EventsListViewModel.cs
+++ Traveler/BL/ViewModels/Planning/EventsListViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Traveler.BL.ViewModels.Planning
 {
     class EventsListViewModel : BaseViewModel
     {
+        public ICommand GoToEventDescriptionCommand => GetNavigateToCommand(AppPages.EventDescription);
+        public ICommand GoToEventNameCommand => GetNavigateToCommand(AppPages.EventName);
     }
 }
