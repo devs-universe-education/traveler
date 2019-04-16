2b7cdc7a37732dc397a2085ad329436bd46a1ffa
Sophia Prutkova <sof-prutkova@yandex.ru>
2019-04-16 21:26:13 +0300

    Added ListEvent Mock

--- Traveler/BL/ViewModels/Planning/EventsListViewModel.cs
+++ Traveler/BL/ViewModels/Planning/EventsListViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using Traveler.DAL.DataObjects;
+using Traveler.DAL.DataServices;
 
 namespace Traveler.BL.ViewModels.Planning
 {
@@ -9,5 +12,26 @@ namespace Traveler.BL.ViewModels.Planning
     {
         public ICommand GoToEventDescriptionCommand => MakeNavigateToCommand(AppPages.EventDescription);
         public ICommand GoToEventNameCommand => MakeNavigateToCommand(AppPages.EventName);
+
+        public DayDataObject ListEventObject
+        {
+            get => Get<DayDataObject>();
+            private set => Set(value);
+        }
+
+
+        public override async Task OnPageAppearing()
+        {
+            State = PageState.Loading;
+            var result = await DataServices.MainTravel.GetDayDataObject(CancellationToken);
+            if (result.IsValid)
+            {
+                ListEventObject = result.Data;
+                State = PageState.Normal;
+            }
+            else
+                State = PageState.Error;
+        }
+
     }
 }
