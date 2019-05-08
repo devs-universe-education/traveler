d0b04a7f28570548defd73f5c8d6c217453ecfc1
Sophia Prutkova <sof-prutkova@yandex.ru>
2019-05-08 21:08:44 +0300

    requests added

--- Traveler.DAL/dbSQLite/DatabaseTraveler.cs
+++ Traveler.DAL/dbSQLite/DatabaseTraveler.cs
@@ -22,9 +22,28 @@ namespace Traveler.DAL.dbSQLite
             database.CreateTableAsync<Event>().Wait();
         }
 
-        public Task<List<Travel>> GetItemTravelAync()
+        public Task<List<Travel>> GetItemsTravelAync()
         {
             return database.Table<Travel>().ToListAsync();
+        }
+
+        public Task<List<Day>> GetItemsDayAsync()
+        {
+            return database.Table<Day>().ToListAsync();
+        }
+
+        public Task<List<Event>> GetItemsEventAsync()
+        {
+            return database.Table<Event>().ToListAsync();
+        }
+
+        public Task<List<Event>> GetEventsOfTheDayAsync(int IdTrav, DateTime date)
+        {
+            var IdDay = database.QueryAsync<Day>("SELECT [ID] FROM [Days] " +
+                "WHERE [IdTravel] = {0}  AND [Date] = {1}", IdTrav, date);
+
+            return database.QueryAsync<Event>("SELECT * FROM [Events]" +
+                "WHERE [IdDay] = {0}", IdDay);
         }
     }
 }
