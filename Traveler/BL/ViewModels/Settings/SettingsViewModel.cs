9d5c8bf92a7f48ecb4e71eea87eb41bc8133fb98
temich335 <temich335@yandex.ru>
2019-05-24 17:32:32 +0300

    Add Picker in Settings

--- Traveler/BL/ViewModels/Settings/SettingsViewModel.cs
+++ Traveler/BL/ViewModels/Settings/SettingsViewModel.cs
@@ -6,5 +6,26 @@ namespace Traveler.BL.ViewModels.Settings
 {
     class SettingsViewModel : BaseViewModel
     {
+        public List<string> ThemeList
+        {
+            get => Get<List<string>>();
+            private set => Set(value);
+        }
+
+        public string ThemeItem
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public SettingsViewModel()
+        {
+            ThemeList = new List<string>()
+            {
+                "Светлая тема",
+                "Темная тема"
+            };
+           
+        }
     }
 }
